4a84864c33f5a7ea424e4dc226d72e35582d041a
ShunjiHashimoto <h.shunji1206@gmail.com>
2024-08-16 18:48:37 +0900

    加減速をなめらかにする

--- tang_control/tang_control/motor.py
+++ tang_control/tang_control/motor.py
@@ -15,27 +15,53 @@ class Motor:
         self.r_REV = LED(Pin.direction_r_REV)
         self.l_FWD = LED(Pin.direction_l_FWD)
         self.l_REV = LED(Pin.direction_l_REV)
+        # 過去のジョイスティック入力を記憶するための変数を追加
+        self.prev_normalized_value_x = 0
+        self.prev_normalized_value_y = 0
+        self.alpha = 0.1  # EMAの平滑化係数。0に近いほど変化が緩やか
     
-    def normalize_joystick_input(self, value, max_value=PWM.max_duty):
-        return value*max_value
+    def normalize_joystick_input(self, value, max_value=PWM.max_duty, prev_value=0):
+        # ジョイスティック入力がゼロなら、ゼロを返す
+        # if abs(value) <= 0.0000001:
+        #     return 0
+        # ジョイスティック入力を正規化
+        normalized_value = value * max_value
+        # 平滑化された値を計算
+        smoothed_value = self.alpha * normalized_value + (1 - self.alpha) * prev_value
+        return smoothed_value
     
     def calc_duty_by_joyinput(self, joystick_x, joystick_y):
-        x = self.normalize_joystick_input(joystick_x)
-        y = self.normalize_joystick_input(joystick_y)
-        if(y > 0):
+        x = self.normalize_joystick_input(joystick_x, prev_value=self.prev_normalized_value_x)
+        y = self.normalize_joystick_input(joystick_y, prev_value=self.prev_normalized_value_y)
+        # 現在の値を保存しておく
+        self.prev_normalized_value_x = x
+        self.prev_normalized_value_y = y
+        # その場旋回を実現するためのロジック
+        print(f"joystick: {joystick_x}, {joystick_y}")
+        if abs(joystick_y) < 0.05 and abs(joystick_x) > 0.90:  # ジョイスティックが真横に倒されている場合
+            print(f"x: {x}")
             if(x > 0):
-                duty_r = y 
-                duty_l = ((PWM.max_duty - x)/PWM.max_duty)*y
-            if(x <= 0):
-                duty_r = ((PWM.max_duty + x)/PWM.max_duty)*y
-                duty_l = y
-        elif(y <= 0):
-            if(x > 0):
-                duty_r = y 
-                duty_l = -abs(((PWM.max_duty - x)/PWM.max_duty)*y)
-            if(x <= 0):
-                duty_l =  y
-                duty_r = -abs(((PWM.max_duty + x)/PWM.max_duty)*y)
+                duty_r = x
+                duty_l = 0
+            else:
+                duty_r = 0
+                duty_l = -x
+        else:
+            if(y > 0):
+                if(x > 0):
+                    duty_r = y 
+                    duty_l = ((PWM.max_duty - x)/PWM.max_duty)*y
+                if(x <= 0):
+                    duty_r = ((PWM.max_duty + x)/PWM.max_duty)*y
+                    duty_l = y
+                    print(f"duty_r: {duty_r}, duty_l: {duty_l}")
+            elif(y <= 0):
+                if(x > 0):
+                    duty_r = y 
+                    duty_l = -abs(((PWM.max_duty - x)/PWM.max_duty)*y)
+                if(x <= 0):
+                    duty_l =  y
+                    duty_r = -abs(((PWM.max_duty + x)/PWM.max_duty)*y)
         duty_l = max(min(duty_l, PWM.max_duty), -PWM.max_duty)
         duty_r = max(min(duty_r, PWM.max_duty), -PWM.max_duty)
         return duty_r, duty_l
@@ -61,22 +87,26 @@ class Motor:
         return duty_r, duty_l
         
     def pwm_control_r_FWD(self, duty):
-        self.r_FWD.on()
+        # self.r_FWD.on()
+        self.r_FWD.off()
         self.r_REV.off()
         self.r_pwm.value = duty
         return
     def pwm_control_r_REV(self, duty):
-        self.r_FWD.off()
+        self.r_FWD.on()
+        # self.r_FWD.off()
         self.r_REV.on()
         self.r_pwm.value = duty
         return
     def pwm_control_l_FWD(self, duty):
-        self.l_FWD.on()
+        # self.l_FWD.on()
+        self.l_FWD.off()
         self.l_REV.off()
         self.l_pwm.value = duty
         return
     def pwm_control_l_REV(self, duty):
-        self.l_FWD.off()
+        self.l_FWD.on()
+        # self.l_FWD.off()
         self.l_REV.on()
         self.l_pwm.value = duty
         return
