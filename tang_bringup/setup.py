e86f692abf36848a7219e63235b03afe1a7553d0
Shunji.Hashimoto <h.shunji1206@gmail.com>
2025-04-18 00:16:47 +0900

    【BLDC, DC】複数ノードの同時立ち上げ

--- tang_bringup/setup.py
+++ tang_bringup/setup.py
@@ -1,25 +1,25 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'tang_bringup'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    version='0.0.1',
+    packages=[package_name],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='root@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='user',
+    maintainer_email='user@example.com',
+    description='Launch files for tang_ros2',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
+    data_files=[
+        (os.path.join('share', package_name), ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+    ],
 )
