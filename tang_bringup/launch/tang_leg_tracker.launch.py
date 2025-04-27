23edcc3cdf6407be5ad0ad14731846018140d47e
ShunjiHashimoto <h.shunji1206@gmail.com>
2025-04-27 23:48:05 +0900

    【BLDC, DC】static_tfを追加

--- tang_bringup/launch/tang_leg_tracker.launch.py
+++ tang_bringup/launch/tang_leg_tracker.launch.py
@@ -15,8 +15,29 @@ def generate_launch_description():
         )
     )
     
+    static_baselink_tf = ExecuteProcess(
+        cmd=[
+            "ros2", "run", "tf2_ros", "static_transform_publisher",
+            "0", "0", "0",  "0", "0", "0",
+            "map", "base_link"
+        ],
+        output="screen"
+    )
+    
+    static_laser_tf = ExecuteProcess(
+        cmd=[
+            "ros2", "run", "tf2_ros", "static_transform_publisher",
+            "0.0", "0", "0.0",         # 位置オフセット (x, y, z)
+            "0", "0", "3.1415",     # 姿勢オフセット (roll, pitch, yaw) ← Z軸回り180度回転
+            "base_link", "laser"    # 親フレーム base_link, 子フレーム laser
+        ],
+        output="screen"
+    )
+    
     return LaunchDescription([
         urg_launch,
+        static_baselink_tf,
+        static_laser_tf,
         ExecuteProcess(
             cmd=["ros2", "run", "icart_mini_leg_tracker", "leg_cluster_tracking_node"],
             output="screen",
