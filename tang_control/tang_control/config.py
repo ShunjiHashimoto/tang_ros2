b4f157123e43d63b29b2404cbfafe2cd808993bc
ShunjiHashimoto <h.shunji1206@gmail.com>
2025-04-27 23:50:27 +0900

    【BLDC, DC】追従用の最大速度パラメータを追加

--- tang_control/tang_control/config.py
+++ tang_control/tang_control/config.py
@@ -71,6 +71,7 @@ class PWM:
     # PWM周波数をHzで指定
     frequency = 500 # [Hz]
     max_duty = 0.15
+    max_duty_follow = 0.2
     min_duty = 0.05
     turn_const_duty_r = 0.1 # 右車輪の定数デューティ比, 超信地旋回時
     turn_const_duty_l = 0.1 # 左車輪の定数デューティ比
